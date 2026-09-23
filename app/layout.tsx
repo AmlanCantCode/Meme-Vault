d9a8de9b20a17d54c0b82f445ddab1d16537c2f9
Amlan <164752796+MoboTheProgrammer@users.noreply.github.com>
2026-09-23 17:58:06 +0530

    feat: integrate monetag in-page push and vignette ad zones

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +29,22 @@ export default function RootLayout({ children }: LayoutProps) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <head>
+        {/* Monetag In-Page Push Zone (11870384) */}
+        <Script
+          src="https://alwingulla.com/88/tag.min.js"
+          data-zone="11870384"
+          strategy="afterInteractive"
+        />
+        
+        {/* Monetag Vignette Banner Zone (11870472) */}
+        <Script
+          src="https://alwingulla.com/88/tag.min.js"
+          data-zone="11870472"
+          strategy="afterInteractive"
+        />
+      </head>
+      <body className="min-h-full flex flex-col bg-neutral-950 text-white">
         {children}
         <Analytics />
       </body>
